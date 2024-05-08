18f0aee9d1be3a5c02e6c0759897c9636ed96a81
TylerBaze03 <146395289+TylerBaze03@users.noreply.github.com>
2024-05-08 16:38:09 -0400

    Delete LeetcodeStuff/TwoSum.py

--- LeetcodeStuff/TwoSum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            t = (nums[i]+nums[i+1])
-            if (t) == target:
-                ans = []
-                ans.append(i)
-                ans.append(i+1)
-                return ans
