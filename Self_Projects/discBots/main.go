11063d644dbbc581e2fe061d28c8a0a7b4b760d2
tylerbaze03@gmail.com <146395289+TylerBaze03@users.noreply.github.com>
2025-01-14 01:37:25 -0500

    sec

--- Self_Projects/discBots/main.go
+++ Self_Projects/discBots/main.go
@@ -4,7 +4,7 @@ import bot "noBot/bot"
 
 func main() {
 
-	bot.BotToken = "MTMxNjUzOTY2NzkxODYyMjc5Mg.GMt93J.o_JzuM1PceilKtQk1ALpvM44ShfZy5qvOEV0LE"
+	bot.BotToken = "xxx"
 	bot.Run() // call the run function of bot/bot.go
 
 }
